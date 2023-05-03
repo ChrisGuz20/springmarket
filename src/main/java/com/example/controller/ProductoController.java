8a8b723af5ed1d0943fd4f21b416c0d019264d2b
ChrisGuz <chguzman20@gmail.com>
2023-05-03 15:24:40 -0600

    Funcion guardar producto, vista crear y logger para productocontroller

--- src/main/java/com/example/controller/ProductoController.java
+++ src/main/java/com/example/controller/ProductoController.java
@@ -1,15 +1,40 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.*;
+import com.example.model.*;
+import com.example.services.*;
+
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 	
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+	
+	@Autowired
+	private ProductoServices productoService;
+	
+	
 	@GetMapping("")
 	public String mostrar() {
 		return"productos/show";
+	}
+	
+	@GetMapping("/create")
+	public String crear() {
+		return"productos/create";
+	}
+	
+	@PostMapping("/save")
+	public String guardar(Producto producto) {
+		LOGGER.info("Este es el objeto producto {}",producto);
+		Usuario u = new Usuario(1,"","","","","","","");
+		producto.setUsuario(u);
+		productoService.guardar(producto);
+		return"redirect:/productos";
 	}
 	
 }
