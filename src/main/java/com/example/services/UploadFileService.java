d0988e293d5979345f51fba1849d383186b4d36e
ChrisGuz <chguzman20@gmail.com>
2023-05-03 18:13:50 -0600

    Clase subir imagen en bytes a servidor

---
+++ src/main/java/com/example/services/UploadFileService.java
@@ -0,0 +1,28 @@
+package com.example.services;
+
+import java.io.IOException;
+import java.nio.file.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.multipart.*;
+import java.io.File;
+
+@Service
+public class UploadFileService {
+	private String carpeta="images//";
+	
+	public String saveImage(MultipartFile file) throws IOException {
+		if(!file.isEmpty()) {
+			byte[] bytes=file.getBytes();
+			Path path= Paths.get(carpeta+file.getOriginalFilename());
+			Files.write(path, bytes);
+			return file.getOriginalFilename();
+		}
+		return "default.jpg";
+	}
+	
+	public void deleteImage(String nombre) {
+		String ruta="images//";
+		File file=new File(ruta+nombre);
+		file.delete();
+	}
+}
