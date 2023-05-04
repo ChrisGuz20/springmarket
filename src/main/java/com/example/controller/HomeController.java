500b64127cf84369e6c43e02c5c1525aee0bb4e3
ChrisGuz <chguzman20@gmail.com>
2023-05-03 20:38:38 -0600

    Funcion ver descripcion del producto

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+import org.slf4j.*;
 import com.example.services.*;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log=LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private ProductoServices productoservice;
@@ -18,5 +20,12 @@ public class HomeController {
 	public String inicio(Model model) {
 		model.addAttribute("productos", productoservice.findAll());
 		return "usuarios/home";
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		log.info("Id del producto enviado como parametro {}",id);
+		
+		return "usuarios/productohome";
 	}
 }
