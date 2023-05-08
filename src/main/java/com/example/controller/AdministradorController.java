c143dc569be267c69378548095417848834a526f
ChrisGuz <chguzman20@gmail.com>
2023-05-07 21:23:45 -0600

    mostrar todas las ordenes de compra en administrador

--- src/main/java/com/example/controller/AdministradorController.java
+++ src/main/java/com/example/controller/AdministradorController.java
@@ -20,6 +20,9 @@ public class AdministradorController {
 	@Autowired
 	private IUsuarioService usuarioService;
 	
+	@Autowired
+	private IOrdenService ordenService;
+	
 	@GetMapping("")
 	public String home(Model model) {
 		List<Producto> productos= productoService.findAll();
@@ -31,5 +34,12 @@ public class AdministradorController {
 	public String usuarios(Model model) {
 		model.addAttribute("usuarios", usuarioService.findAll());
 		return "administrador/usuarios";
+	}
+	
+	@GetMapping("/ordenes")
+	public String ordenes(Model model) {
+		model.addAttribute("usuarios", usuarioService.findAll());
+		model.addAttribute("ordenes", ordenService.findAll());
+		return "administrador/ordenes";
 	}
 }
