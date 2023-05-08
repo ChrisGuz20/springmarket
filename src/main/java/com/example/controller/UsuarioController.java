9c4fca4ce95208586be2c463ad625aaaa888b64a
ChrisGuz <chguzman20@gmail.com>
2023-05-07 20:27:12 -0600

    funcion logout activa

--- src/main/java/com/example/controller/UsuarioController.java
+++ src/main/java/com/example/controller/UsuarioController.java
@@ -87,5 +87,11 @@ public class UsuarioController {
 		model.addAttribute("sesion", session.getAttribute("idusuario"));
 		return "usuario/detallecompra";
 	}
+	
+	@GetMapping("/cerrar")
+	public String cerrarSesion(HttpSession session) {
+		session.removeAttribute("idusuario");
+		return"redirect:/";
+	}
 
 }
